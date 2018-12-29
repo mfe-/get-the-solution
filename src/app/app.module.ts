589be07045df585a188f66ffb999ff8ce88a8d0b
Martin Fetzel <martin.fetzel@get-the-solution.net>
2018-12-29 10:55:31 +0100

    added header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HeaderComponent } from './header.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/header.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, TemplateRef, Inject } from '@angular/core';
+
+@Component({
+  selector: 'my-header',
+  templateUrl: './header.component.html'
+})
+export class HeaderComponent {
+
+    constructor() {
+
+    };
+    title = 'get-the-solution';
+
+}

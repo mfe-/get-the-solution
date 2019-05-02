6e9850554054c757b8ae1ba56258d833b1c07f4e
Martin Fetzel <martin.fetzel@get-the-solution.net>
2019-05-02 21:12:18 +0200

    Forward old links close #6

--- src/app/components/page-not-found.component.ts
+++ src/app/components/page-not-found.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { IBlogService } from '../contract/IBlogService';
+import { BlogEntry } from '../model/BlogEntry';
 
 @Component({
   selector: 'app-page-not-found',
@@ -7,18 +9,37 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: []
 })
 export class PageNotFoundComponent implements OnInit {
-
-  constructor(private route: ActivatedRoute, private router: Router) { }
+  private readonly REGEXLINK: string = "index-blog-[0-9]+-[0-9]+-([0-9]+)-.+";
+  constructor(private route: ActivatedRoute, private router: Router, @Inject('IBlogService') private blogService: IBlogService) { }
 
   ngOnInit() {
     let id = this.route.snapshot.paramMap.get('p');
+    let autoforward = true;
     if (id == null) {
       this.Link = "/home";
     }
     else {
+      //check if the link was from the predecessor website get-the-solution.net v1
+      let regeex = new RegExp(this.REGEXLINK);
+      if (regeex.test(id)) {
+        let regexmatch = regeex.exec(id);
+        if (regexmatch.length > 1) {
+          //we found the id
+          let blogid = +regexmatch[1];
+          autoforward = false;
+          this.blogService.GetBlogEntries(false).subscribe((b: BlogEntry[]) => {
+            let blogentry: BlogEntry = b.find((blog) => blog.BlogEntryId == blogid);
+            this.Link = "/blog/" + blogentry.Title;
+            this.router.navigate([this.Link]);
+          });
+        }
+      }
+
       this.Link = "/" + this.route.snapshot.paramMap.get('p');
     }
-    this.router.navigate([this.Link]);
+    if (autoforward) {
+      this.router.navigate([this.Link]);
+    }
   }
   public Link: string;
 

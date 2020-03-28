a457583ba24c57682118480a8e2d78b96a7b1e4d
Martin Fetzel <martin.fetzel@get-the-solution.net>
2020-03-28 08:14:13 +0100

    set blog title as window title

--- src/app/components/blog/blogEntry.component.ts
+++ src/app/components/blog/blogEntry.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, Inject, Input } from '@angular/core';
 import { IBlogService } from 'src/app/contract/IBlogService';
 import { BlogEntry } from 'src/app/model/BlogEntry';
 import { environment } from 'src/environments/environment';
+import {Title} from "@angular/platform-browser";
 
 @Component({
     selector: 'app-blogentry',
@@ -10,12 +11,21 @@ import { environment } from 'src/environments/environment';
   })
 export class BlogEntryComponent implements OnInit {
 
-    @Input()
-    blogEntryValue: BlogEntry;
 
-    constructor(@Inject('IBlogService') private blogService: IBlogService) {
+    private _blogEntryValue: BlogEntry;
+
+    constructor(@Inject('IBlogService') private blogService: IBlogService, private titleService:Title) {
 
     }
+
+    @Input() set blogEntryValue(value: BlogEntry) {
+      this._blogEntryValue = value;
+      this.titleService.setTitle("get-the-solution.net - "+ value.Title );
+    }
+    get blogEntryValue(): BlogEntry {
+      return this._blogEntryValue;
+    }
+
     public ngOnInit() {
     }
 

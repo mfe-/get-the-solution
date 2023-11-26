f20e20ca00912d3b33f3c6718bb7915cb6a04a11
Martin Fetzel <U01K1RM@post.at>
2023-11-26 17:46:41 +0100

    move pandoc generated html files to src folder

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   git: "https://github.com/mfe-/get-the-solution/edit/master/src/",
-  baseUri: "https://get-the-solution.net/"
+  baseUri: "https://get-the-solution.azurewebsites.net/"
 };

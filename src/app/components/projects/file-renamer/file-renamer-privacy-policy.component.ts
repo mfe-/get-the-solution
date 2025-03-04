890fbb9ebc2cd933e022a33a2eb5e59767c98ea2
Martin <martin.fetzel@get-the-solution.net>
2025-03-04 21:41:33 +0100

    Add File Renamer privacy policy component and update routing (#113)
    * Add File Renamer privacy policy component and update routing
    
    * Update GitHub Actions to use upload-artifact@v4
    
    * Update GitHub Actions to use download-artifact@v4
    
    * Update GitHub Actions to use upload-artifact@v4

---
+++ src/app/components/projects/file-renamer/file-renamer-privacy-policy.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-file-renamer-privacy-policy',
+  imports: [],
+  templateUrl: './file-renamer-privacy-policy.component.html',
+})
+export class FileRenamerPrivacyPolicyComponent {
+
+}
